b404c42413c08076c338d7d459259efbfc195d85
M Yusuf Mukharom <42793951+051mym@users.noreply.github.com>
2019-03-05 20:46:08 +0700

    kucing

--- head.php
+++ head.php
@@ -17,4 +17,9 @@
 	
 	<link rel="stylesheet" href="css/style.css" />
 	<link rel="stylesheet" href="css/responsive.css" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+
+</head>
 </head>
