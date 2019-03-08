c5b113353648d7bf18d8b02940ad5c42571e67ea
M Yusuf Mukharom <42793951+051mym@users.noreply.github.com>
2019-03-08 22:19:22 +0700

    awd

--- regis.php
+++ regis.php
@@ -18,7 +18,7 @@ include 'config.php';
 				<div class="main_featured_content">
 					<div class="col-md-4 col-sm-6 col-xs-12 single_featured_area">
 						<div class="single_featured wow fadeIn" data-wow-duration=".5s">
-							<img src="images/f1.jpg" alt="" />
+							<img src="images/f1.png" alt="" height="270"/>
 							<div class="featured_overlay">
 								<div class="overlay_content">
 									<input type="Submit" class="btn btn-white btn-link" value="Mahasiswa" name="regis" >
@@ -28,7 +28,7 @@ include 'config.php';
 					</div>
 					<div class="col-md-4 col-sm-6 col-xs-12 single_featured_area">
 						<div class="single_featured wow fadeIn" data-wow-duration=".5s">
-							<img src="images/f2.jpg" alt="" />
+							<img src="images/f2.png" alt="" height="270" />
 							<div class="featured_overlay">
 								<div class="overlay_content">
 									<input type="Submit" class="btn btn-white btn-link" value="Dosen" name="regis" >
@@ -38,7 +38,7 @@ include 'config.php';
 					</div>
 					<div class="col-md-4 col-sm-6 col-xs-12 single_featured_area">
 						<div class="single_featured wow fadeIn" data-wow-duration=".5s">
-							<img src="images/f3.jpg" alt="" />
+							<img src="images/f3.png" alt="" height="270" />
 							<div class="featured_overlay">
 								<div class="overlay_content">
 									<input type="Submit" class="btn btn-white btn-link" value="Perusahaan" name="regis" >
